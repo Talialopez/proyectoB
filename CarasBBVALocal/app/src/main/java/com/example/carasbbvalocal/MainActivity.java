fb093c96e64553b0a0447d31b0bd078d771e61ec
Joshua Sedano Rodríguez <114091298+JoshuaSedano@users.noreply.github.com>
2023-12-14 11:26:26 +0100

    reparar carpetas

---
+++ CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.carasbbvalocal;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import com.example.carasbbvalocal.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int score = 0;
+    private TextView tvScore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onOkClick(View view) {
+        score += 3;
+        logScore();
+    }
+
+    public void onNeutralClick(View view) {
+        score += 2;
+        logScore();
+    }
+
+
+    public void onDisagreementClick(View view) {
+        score += 1;
+        logScore();
+    }
+
+    private String getFormattedDateTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        return sdf.format(new Date());
+    }
+
+    private void logScore() {
+        String formattedDateTime = getFormattedDateTime();
+        Log.d("prueba", ("Current Score: " + score + " | Date and Time: " + formattedDateTime));
+    }
+}
