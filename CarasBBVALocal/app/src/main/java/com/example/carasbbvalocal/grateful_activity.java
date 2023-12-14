8833ecb8ff308efbdcd3b68d409b6c2ca0be925e
Talia <talialopezarias@gmail.com>
2023-12-14 11:56:18 +0100

    Temporizador y land

---
+++ CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/grateful_activity.java
@@ -0,0 +1,14 @@
+package com.example.carasbbvalocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class grateful_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grateful_land);
+    }
+}
