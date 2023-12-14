8ef0f4469359119fa152f9c3270f72eff1c7d6bb
Joshua Sedano Rodríguez <114091298+JoshuaSedano@users.noreply.github.com>
2023-12-14 13:03:52 +0100

    código flujo entre pantallas

--- CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/grateful_activity.java
+++ CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/grateful_activity.java
@@ -2,13 +2,31 @@ package com.example.carasbbvalocal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class grateful_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grateful_land);
-    }
+
+        int orientation = getResources().getConfiguration().orientation;
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_grateful_land);
+        } else {
+            setContentView(R.layout.activity_grateful);
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(grateful_activity.this , MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+   }
+
 }
