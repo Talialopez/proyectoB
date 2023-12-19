2f4fed7ed555211ff48478afe6ca50b0c6a7ed5f
Talia <talialopezarias@gmail.com>
2023-12-19 12:19:25 +0100

    Actualizaciones

--- CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/MainActivity.java
+++ CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/MainActivity.java
@@ -2,16 +2,15 @@ package com.example.carasbbvalocal;
 
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -21,53 +20,81 @@ public class MainActivity extends AppCompatActivity {
 
     private int score = 0;
     private int voto = 0;
-    private TextView tvScore;
-
-    private TextView tittleEn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         int orientation = getResources().getConfiguration().orientation;
-        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             setContentView(R.layout.activity_main_land);
         } else {
             setContentView(R.layout.activity_main);
         }
 
-        tittleEn = findViewById(R.id.tittle);
+        Spinner languageSpinner = findViewById(R.id.languageSpinner);
+
+        // Opciones Spinner creando un array de languages
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.languages,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
+
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedLanguage = (String) parentView.getItemAtPosition(position);
+                updateTexts(selectedLanguage);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
     }
 
-    private void actualizarResource(String idioma) {
-        Resources recursos = getResources();
-        Configuration configuracion = recursos.getConfiguration();
-        Locale locale = new Locale(idioma);
-        configuracion.setLocale(locale);
-        recursos.updateConfiguration(configuracion, recursos.getDisplayMetrics());
+    private void updateTexts(String selectedLanguage) {
+        TextView titleTextView = findViewById(R.id.tittle);
 
-        tittleEn.setText(recursos.getString(R.string.tittle));
+        // ID segun el idioma seleccionado
+        int titleResourceId;
+        if (selectedLanguage.equalsIgnoreCase("Inglés")) {
+            titleResourceId = R.string.tittle_ingles;
+        } else if (selectedLanguage.equalsIgnoreCase("Gallego")) {
+            titleResourceId = R.string.tittle_gallego;
+        } else if (selectedLanguage.equalsIgnoreCase("Catalán")) {
+            titleResourceId = R.string.tittle_catalan;
+        } else if (selectedLanguage.equalsIgnoreCase("Euskera")) {
+            titleResourceId = R.string.tittle_euskera;
+        } else {
+            // Por defecto, título en español
+            titleResourceId = R.string.tittle;
+        }
 
+        // Actualizar el texto de la pantalla
+        titleTextView.setText(titleResourceId);
     }
 
     public void onOkClick(View view) {
         score += 3;
-        voto=3;
-        logScore();
-        lanzarGrateful();
-    }
-    
-    public void onNeutralClick(View view) {
-        score += 2;
-        voto=2;
+        voto = 3;
         logScore();
         lanzarGrateful();
     }
 
+    public void onNeutralClick(View view) {
+        score += 2;
+        voto = 2;
+        logScore();
+        lanzarGrateful();
+    }
 
     public void onDisagreementClick(View view) {
         score += 1;
-        voto=1;
+        voto = 1;
         logScore();
         lanzarGrateful();
     }
@@ -79,16 +106,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void logScore() {
         String formattedDateTime = getFormattedDateTime();
-        Log.d("prueba", ("Su voto: " + voto +" | Puntuación total: " + score + " | Date and Time: " + formattedDateTime));
+        android.util.Log.d("prueba", ("Su voto: " + voto + " | Puntuación total: " + score + " | Date and Time: " + formattedDateTime));
     }
 
-  
     private void lanzarGrateful() {
-
-                Intent intent = new Intent(MainActivity.this, grateful_activity.class);
-                startActivity(intent);
-                finish();
+        Intent intent = new Intent(MainActivity.this, grateful_activity.class);
+        startActivity(intent);
+        finish();
     }
 }
-
-
