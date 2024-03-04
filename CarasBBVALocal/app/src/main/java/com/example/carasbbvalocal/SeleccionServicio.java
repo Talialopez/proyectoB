01f2c1003a27ac119c8dbb821a3feca2c2eb04ba
Joshua Sedano Rodríguez <114091298+JoshuaSedano@users.noreply.github.com>
2024-03-04 11:48:45 +0100

    orden de actividades cambiado y orientación en horizontal

--- CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/SeleccionServicio.java
+++ CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/SeleccionServicio.java
@@ -2,6 +2,7 @@ package com.example.carasbbvalocal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class SeleccionServicio extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class SeleccionServicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_servicio);
+
+        // Establecer la orientación en horizontal
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
