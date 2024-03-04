add9ad74d970b81d0cd63e38fa21359fda2bc9c9
Joshua Sedano Rodríguez <114091298+JoshuaSedano@users.noreply.github.com>
2024-03-04 11:29:38 +0100

    diseño layout seleccion de servicios

---
+++ CarasBBVALocal/app/src/main/java/com/example/carasbbvalocal/SeleccionServicio.java
@@ -0,0 +1,14 @@
+package com.example.carasbbvalocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeleccionServicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_servicio);
+    }
+}
